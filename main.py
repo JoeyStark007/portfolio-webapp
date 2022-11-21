db58da71774d5bc865c07d237aa6d74c53cbdbfe
JoeyStark007 <jgtefera@gmail.com>
2022-11-21 18:30:53 -0500

    Created columns and coded image and description.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+col1, col2 = st.columns(2)
 
+with col1:
+    st.image("images/photo.jpg", width=400)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Joseph Tefera")
+    content = """
+    Hi, My name is Joseph Tefera. I am an aspiring python programmer. I am a Financial Technology
+    major in VCU minoring in Math and Statistics looking to graduate in May 2023. My IT learning began
+    in high school from my coding classes but began to sprout when I started to compete in hackathons
+    hosted at my college and at Virginia Tech. I have worked for JB Hunt Logistics as well as 
+    Major Leage Hacking fellowship. I still have a lot to learn and I look forward to it.
+    
+    """
+    st.info(content)
